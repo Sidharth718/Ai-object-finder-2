497e7800b142592d8538f02d5b02cdc88f41eb19
Sidharth718 <71827514+Sidharth718@users.noreply.github.com>
2022-04-15 14:38:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+Status = "";
+input_text = "";
+
+function setup(){
+    canvas = createCanvas(400,400);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(400,400);
+    video.hide();
+}
+function start(){
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    input_text = document.getElementById("input_id").value;
+}
+function modelLoaded(){
+    console.log("Model is loaded!");
+    Status = true;
+}
+function draw(){
+    image(video,0,0,400,400);
+}
